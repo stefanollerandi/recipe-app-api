7c7ac6ea743897ea1c1756a9e39a67d2dafbcc4c
stefanollerandi <stefano.llerandi@studbocconi.it>
2024-11-10 10:10:44 -0600

    Fixed linting.  Please enter the commit message for your changes. Lines starting

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,9 +12,11 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
+
 
 class PublicUserApiTests(TestCase):
     """Test the public features of the user API."""
@@ -25,9 +27,9 @@ class PublicUserApiTests(TestCase):
     def test_create_user_success(self):
         """Test creating a user is successful."""
         payload = {
-            'email' : 'test@example.com',
+            'email': 'test@example.com',
             'password': 'testpass123',
-            'name' : 'Test Name',
+            'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -40,8 +42,8 @@ class PublicUserApiTests(TestCase):
         """Test error returned if user with email exists."""
         payload = {
             'email': 'test@example.com',
-            'password' : 'testpass123',
-            'name' : 'Test Name',
+            'password': 'testpass123',
+            'name': 'Test Name',
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
@@ -51,9 +53,9 @@ class PublicUserApiTests(TestCase):
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
         payload = {
-            'email' : 'test@example.com',
-            'password' : 'pw',
-            'name' : 'Test name',
+            'email': 'test@example.com',
+            'password': 'pw',
+            'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -65,9 +67,9 @@ class PublicUserApiTests(TestCase):
     def test_create_token_for_user(self):
         """Test generates token for valid credentials."""
         user_details = {
-            'name':'Test Name',
+            'name': 'Test Name',
             'email': 'test@example.com',
-            'password' : 'test-user-password123',
+            'password': 'test-user-password123',
         }
         create_user(**user_details)
 
@@ -84,7 +86,7 @@ class PublicUserApiTests(TestCase):
         """Test returns error if credentials invalid."""
         create_user(email='test@example.com', password='goodpass')
 
-        payload = {'email': 'test@example.com','password': 'badpass'}
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
@@ -111,7 +113,7 @@ class PrivateUserApiTests(TestCase):
     def setUp(self):
         self.user = create_user(
             email='text@example.com',
-            password = 'testpass123',
+            password='testpass123',
             name='Test Name',
         )
         self.client = APIClient()
@@ -135,7 +137,7 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
-        payload = {'name': 'Updated name', 'password':'newpassword123'}
+        payload = {'name': 'Updated name', 'password': 'newpassword123'}
 
         res = self.client.patch(ME_URL, payload)
 
