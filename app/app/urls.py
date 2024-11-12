b95d8d99458fa5cd87baba0a7e58c1e017d67945
stefanollerandi <stefano.llerandi@studbocconi.it>
2024-11-11 23:21:17 -0600

    Linting

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -31,3 +34,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -94,4 +94,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Seriallizer for recipe detial view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
